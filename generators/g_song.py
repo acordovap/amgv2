0ebf847609febebd11804656faed06bdb9ea49b6
Alan Córdova Posadas <acordovap@localhost.localdomain>
2021-03-09 16:59:54 -0600

    generator for song agent

---
+++ generators/g_song.py
@@ -0,0 +1,15 @@
+from jinja2 import Environment, FileSystemLoader
+
+tracks = ["t1", "t2"]
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+#env.lstrip_blocks = True
+#env.rstrip_blocks = True
+#env.trim_blocks = True
+
+template = env.get_template('song.py.jinja')
+
+template.stream(tracks=tracks).dump('agents/song.py')
+#output = template.render(tracks = tracks)
+#print(output)
