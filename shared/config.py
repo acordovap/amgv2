98e86bd72aa7d0768514e217745f6729ea74f74c
Alan Córdova Posadas <acordovap@localhost.localdomain>
2021-03-16 14:03:52 -0600

    minor modification to file

--- shared/config.py
+++ shared/config.py
@@ -13,6 +13,8 @@ MELODY_PITCH_RANGE = [4, 5]
 
 # Other values
 no_songs = 1
+
+# To be removed?
 all_notes = ['C', 'Cb', 'C#', 'D', 'Db', 'D#', 'E', 'Eb', 'E#', 'F', 'Fb', 'F#', 'G', 'Gb', 'G#', 'A', 'Ab', 'A#', 'B', 'Bb', 'B#']
 all_keys = ['Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#']
 all_chords = ['I', 'I7', 'II', 'II7', 'III', 'III7', 'IV', 'IV7', 'V', 'V7', 'VI', 'VI7', 'VII', 'VII7']
