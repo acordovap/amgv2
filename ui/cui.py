0a6c95bdc9e12c014c0f82350e8edca519dbaab5
Alan Córdova Posadas <acordovap@localhost.localdomain>
2021-04-12 21:29:48 -0500

    basic ui (incomplete)

--- ui/cui.py
+++ ui/cui.py
@@ -1,30 +1,48 @@
-# Import the lib
 import py_cui
-
-# # create the CUI object. Will have a 3 by 3 grid with indexes from 0,0 to 2,2
-# root = py_cui.PyCUI(3, 3)
-#
-# # Add a label to the center of the CUI in the 1,1 grid position
-# root.add_label('Hello py_cui!!!', 1, 1)
-#
-# # Start/Render the CUI
-# root.start()
+import mingus.core.keys as keys
 
 class CUI:
     def __init__(self, master: py_cui.PyCUI):
         self.master = master
         pianotext =  """
 │  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │   │
-│  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │   │
 │  │ │ │ │  │  │•│ │♦│ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │  │ │ │ │  │  │ │ │ │ │ │  │   │
 │  └┬┘ └┬┘  │  └┬┘ └┬┘ └┬┘  │  └┬┘ └┬┘  │  └┬┘ └┬┘ └┬┘  │  └┬┘ └┬┘  │  └┬┘ └┬┘ └┬┘  │  └┬┘ └┬┘  │  └┬┘ └┬┘ └┬┘  │   │
-│   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
-│ 3 │   │   │ • │ ♦ │   │   │ 4 │   │   │   │   │   │   │ 5 │   │   │   │   │   │   │ 6 │   │   │   │   │   │   │ 7 │
-└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘"""
-        self.pt = self.master.add_text_block('alan', 0, 0, row_span=4, column_span=13)
-        self.pt.set_text(pianotext)
+│   │   │   │ • │ ♦ │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │   │
+└───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+                                                         c-5                                                         """
+        # │ • │ ♦ │
+
+        #  block_label - pianotext
+        self.bl_piano = self.master.add_block_label(pianotext, 0, 0, row_span = 2, column_span = 13)
+        self.bl_piano.add_text_color_rule('♦', py_cui.RED_ON_BLACK, 'contains', match_type='regex', include_whitespace=True)
+
+        # scroll_menu - Key Signature
+        self.sm_keysig = self.master.add_scroll_menu("Key Signature", 2, 0, column_span=2)
+        self.sm_keysig.add_item_list(keys.keys)
+        self.sm_keysig.add_key_command(py_cui.keys.KEY_ENTER, self.update_notes)
+
+        # scroll_menu - Tempo
+        self.sm_tempo = self.master.add_scroll_menu("Tempo", 2, 2, column_span=2)
+        self.sm_tempo.add_item_list(list(range(60, 240, 10)))
+
+        # scroll_menu - Time Signature
+        self.sm_timesig = self.master.add_scroll_menu("Time Signature", 2, 4, column_span=2)
+        self.sm_timesig.add_item_list(['4/4'])
+
+        # button - progression
+        self.b_prog = self.master.add_button('progression', 2, 6, column_span=2, command=None)
+
+        # scroll_menu - Notes
+        self.sm_notes = self.master.add_scroll_menu("Notes", 3, 0, column_span=2, row_span=3)
+
+    def update_notes(self):
+        #print(str(self.sm_keysig.get))
+        self.sm_notes.clear()
+        self.sm_notes.add_item_list(keys.get_notes(self.sm_keysig.get()[0]))
 
 root = py_cui.PyCUI(11, 13)
+root.toggle_unicode_borders()
 root.set_title('amgv2 - cui')
 
 ui = CUI(root)
