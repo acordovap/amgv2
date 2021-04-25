31520013e7b129bcbfa615ef96c7165c32d3ee55
Alan Córdova Posadas <acordovap@localhost.localdomain>
2021-04-25 10:32:11 -0500

    removed parameter since now this come from json input

--- shared/config.py
+++ shared/config.py
@@ -2,21 +2,3 @@
 XMPP_SERVER = "@localhost"
 OUTPUT_PREFIX = "amg_composition_"
 OUTPUT_FOLDER = "./out/"
-
-# Init parameters
-SONG_LENGTH = 1
-SONG_KEY_SIGNATURE = "C"
-SONG_TIME_SIGNATURE = (4, 4)
-SONG_TEMPO = 120
-PROGRESSIONS = ["I", "IV","V", "I"]
-MELODY_PITCH_RANGE = [4, 5]
-
-# Other values
-no_songs = 1
-
-# To be removed?
-all_notes = ['C', 'Cb', 'C#', 'D', 'Db', 'D#', 'E', 'Eb', 'E#', 'F', 'Fb', 'F#', 'G', 'Gb', 'G#', 'A', 'Ab', 'A#', 'B', 'Bb', 'B#']
-all_keys = ['Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#']
-all_chords = ['I', 'I7', 'II', 'II7', 'III', 'III7', 'IV', 'IV7', 'V', 'V7', 'VI', 'VI7', 'VII', 'VII7']
-inversions = ['first', 'second', 'third']
-all_note_values = [1, 2, 4, 8]
