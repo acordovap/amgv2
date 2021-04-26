31f4514589ce59407b843c4a54eabdf2e937f8b8
Alan Córdova Posadas <acordovap@localhost.localdomain>
2021-04-26 14:48:44 -0500

    bug fixes name of clases mnpt accept dash(-)

--- gen.py
+++ gen.py
@@ -22,7 +22,7 @@ if v:
                     b_list.append(b)
                 # Generate notes agents
                 template = env.get_template('note.py.jinja')
-                template.stream(song=s["s_name"], track=t["t_name"], note=n["n_name"], behavs=b_list).dump('agents/'+s["s_name"]+'_'+t["t_name"]+'_'+n["n_name"]+'.py')
+                template.stream(song=s["s_name"], track=t["t_name"], note=n["n_name"], behavs=b_list).dump('agents/'+s["s_name"]+'_'+t["t_name"]+'_'+n["n_name"].replace('-', '')+'.py')
         # Generate songs agents
         template = env.get_template('song.py.jinja')
         template.stream(song=s).dump('agents/'+s["s_name"]+'.py')
