16df9b8fe06adb2553477fecd3ae90ed0b001a38
Alan Córdova Posadas <acordovap@localhost.localdomain>
2021-05-11 20:08:49 -0500

    Cleaning of grammar rules(no more duplicate code), also updated example3 to match change on grammar for commodin token (from '.' to 'x')

--- grammar/amglang.py
+++ grammar/amglang.py
@@ -7,7 +7,7 @@ music_grammar = r"""
 
     msg    : "(" CHORD? SBARS NUMBER ")"       -> msg
 
-    SBARS  : NUMBER|"."
+    SBARS  : NUMBER|"x"
 
     CHORD  : "majT"|"minT"|"dimT"|"augT"|"susT"
 
@@ -19,43 +19,40 @@ music_grammar = r"""
 class TreeToCode(Transformer):
     @v_args(tree=True)
     def msg(self, tree):
+        chordsdict = {
+            "majT": "major_triad",
+            "minT": "minor_triad",
+            "dimT": "diminished_triad",
+            "augT": "augmented_triad",
+            "susT": "suspended_triad"
+        }
         out=""
         minspace="                    "
         tab="    "
+        n, b, d = None, None, None
+
         if any(c.type == "CHORD" for c in tree.children):
             chord, bars, duration = tree.children
-            chordsdict = {
-                "majT": "major_triad",
-                "minT": "minor_triad",
-                "dimT": "diminished_triad",
-                "augT": "augmented_triad",
-                "susT": "suspended_triad"
-            }
             out += minspace+"raw_note = self.agent.get(\"n_v\").split(\"-\")[0]"+"\n"
             out += minspace+"c = chords."+chordsdict[chord]+"(raw_note)"+"\n"
-            if bars==".":
-                out += minspace+"lstatus = ast.literal_eval(self.agent.get(\"last_know_status\"))"+"\n"
-                out += minspace+"i = random.randint(0, len(lstatus[0][4])-1)"+"\n"
-                out += minspace+"msg.body = \'[ \'+" +"str(c)"+ "+\', ["+"\'+str(i)+\'"+"], "+duration+" ]\'"
-            else:
-                out += minspace+"msg.body = \'[ \'+" +"str(c)"+ "+\', ["+bars+"], "+duration+" ]\'"
+            n = "\'+str(c)+\'"
         else:
             bars, duration = tree.children
-            if bars==".":
-                out += minspace+"lstatus = ast.literal_eval(self.agent.get(\"last_know_status\"))"+"\n"
-                out += minspace+"i = random.randint(0, len(lstatus[0][4])-1)"+"\n"
-                out += minspace+"msg.body = \'[ [\"\'+" +"self.agent.get(\"n_v\")"+ "+\'\"], ["+"\'+str(i)+\'"+"], "+duration+" ]\'"
-            else:
-                out += minspace+"msg.body = \'[ [\"\'+" +"self.agent.get(\"n_v\")"+ "+\'\"], ["+bars+"], "+duration+" ]\'"
+            n = "[\"\'+self.agent.get(\"n_v\")+\'\"]"
+        # from here 'bars' and 'duration' are set, as well as 'n'
+        if bars == "x":
+            out += minspace+"lstatus = ast.literal_eval(self.agent.get(\"last_know_status\"))"+"\n"
+            out += minspace+"i = random.randint(0, len(lstatus[0][4])-1)"+"\n"
+            b = "\'+str(i)+\'"
+        else:
+            b = bars
+        d = duration
+        out += minspace+"msg.body = \'[" +n+ ", [" +b+ "], " +d+ " ]\'"
         return out
 
 parser = Lark(music_grammar, parser='lalr', propagate_positions=False, maybe_placeholders=False, transformer=TreeToCode())
 parse = parser.parse
 
 def test(test_input):
-    # test_input = "(1 5)"
     return parse(test_input)
     # print(parse(test_input).pretty())
-
-# if __name__ == '__main__':
-#     test()
